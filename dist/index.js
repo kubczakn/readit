9d2d8947dd985cd2d3a1f5ba25c55fc4be5b4e92
kubczakn <kubczakn@umich.edu>
2021-05-16 17:19:38 -0400

    setup first graphql query

--- dist/index.js
+++ dist/index.js
@@ -13,15 +13,28 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@mikro-orm/core");
-const Post_1 = require("./entities/Post");
 const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
+const express_1 = __importDefault(require("express"));
+const apollo_server_express_1 = require("apollo-server-express");
+const hello_1 = require("./resolvers/hello");
+const type_graphql_1 = require("type-graphql");
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const post = orm.em.create(Post_1.Post, { title: 'my first post' });
-    yield orm.em.persistAndFlush(post);
-    const posts = yield orm.em.find(Post_1.Post, {});
-    console.log(posts);
+    const app = express_1.default();
+    const apolloServer = new apollo_server_express_1.ApolloServer({
+        schema: yield type_graphql_1.buildSchema({
+            resolvers: [hello_1.HelloResolver],
+            validate: false
+        })
+    });
+    app.get('/', (_, res) => {
+        res.send('Hello');
+    });
+    apolloServer.applyMiddleware({ app });
+    app.listen(4000, () => {
+        console.log('Server listening on port 4000');
+    });
 });
 main().catch(err => {
     console.log(err);
--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,36 @@ import { MikroORM } from "@mikro-orm/core";
 import { __prod__ } from "./constants";
 import { Post } from "./entities/Post";
 import microConfig from './mikro-orm.config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { HelloResolver } from "./resolvers/hello";
+import { buildSchema } from 'type-graphql';
 
 const main = async () => {
+    // Migrations for database schema
     const orm = await MikroORM.init(microConfig);
     await orm.getMigrator().up();
-    const post = orm.em.create(Post, {title: 'my first post'});
-    await orm.em.persistAndFlush(post);
 
-    const posts = await orm.em.find(Post, {});
-    console.log(posts);
+    const app = express();
+    const apolloServer = new ApolloServer({
+        schema: await buildSchema({
+            resolvers: [HelloResolver],
+            validate: false
+        })
+    });
+
+    // Endpoint for our express server
+    app.get('/', (_, res) => {
+        res.send('Hello')
+    });
+
+    // Create graphql endpoint
+    apolloServer.applyMiddleware({ app });
+
+    // Start out express server
+    app.listen(4000, () => {
+        console.log('Server listening on port 4000')
+    });
 };
 
 main().catch(err => {
